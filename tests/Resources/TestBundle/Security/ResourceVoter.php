918a5603526fc7558414d02f594d4f1ed8abfb50
WouterJ <waldio.webdesign@gmail.com>
2017-01-27 17:00:58 +0100

    Prepare 1.0.0-RC1

--- tests/Resources/TestBundle/Security/ResourceVoter.php
+++ tests/Resources/TestBundle/Security/ResourceVoter.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Symfony CMF package.
+ *
+ * (c) 2011-2017 Symfony CMF
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Symfony\Cmf\Bundle\ResourceRestBundle\Tests\Resources\TestBundle\Security;
 
 use Symfony\Cmf\Bundle\ResourceRestBundle\Controller\ResourceController;
